e3ce6ff7ec2388a16ca086ce1bdcb92a5d15040c
Олег Луцик <oleglutsik.dev@gmail.com>
2024-07-05 18:37:31 +0300

    Добавьте файлы проекта.

---
+++ FakeUserGeneratorApp-Log4Net/FakeUser.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+
+namespace FakeUserGeneratorApp
+{
+    public class FakeUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ContactPhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public static FakeUser GenerateFakeUser()
+        {
+            var faker = new Faker();
+
+            return new FakeUser
+            {
+                Name = faker.Name.FirstName(),
+                Surname = faker.Name.LastName(),
+                ContactPhoneNumber = faker.Phone.PhoneNumber(),
+                Email = faker.Internet.Email(),
+                Address = faker.Address.FullAddress()
+            };
+        }
+    }
+}
---
+++ FakeUserGeneratorApp-Log4Net/Program.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using log4net.Config;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+
+namespace FakeUserGeneratorApp
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+            logger.Info("Application started.");
+
+            for (int i = 0; i < 5; i++)
+            {
+                var fakeUser = FakeUser.GenerateFakeUser();
+                logger.Info($"Generated User: {fakeUser.Name} {fakeUser.Surname}, {fakeUser.ContactPhoneNumber}, {fakeUser.Email}, {fakeUser.Address}");
+                Console.WriteLine($"Name: {fakeUser.Name}");
+                Console.WriteLine($"Surname: {fakeUser.Surname}");
+                Console.WriteLine($"Contact Phone Number: {fakeUser.ContactPhoneNumber}");
+                Console.WriteLine($"Email: {fakeUser.Email}");
+                Console.WriteLine($"Address: {fakeUser.Address}");
+                Console.WriteLine("----------------------------");
+            }
+
+            logger.Info("Application finished.");
+        }
+    }
+}
